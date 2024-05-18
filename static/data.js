c16b60b9e515c05ccc578b9c1837f991b813ac9f
Snedit <sohamde2004@gmail.com>
2024-05-18 20:10:06 +0530

    flasked

---
+++ static/data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'static/img/banner/banner1.webp'
+    },
+    {
+        image: 'static/img/banner/banner2.webp'
+    },
+    {
+        image: 'static/img/banner/banner3.webp'
+    },
+    {
+        image: 'static/img/banner/banner4.webp'
+    },
+    {
+        image: 'static/img/banner/banner5.webp'
+    },
+    {
+        image: 'static/img/banner/banner6.webp'
+    }
+]
